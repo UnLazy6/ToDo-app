2985997f8fe665905996e2b30de40d668a1154c6
UnLazy6 <caiovictortv6@gmail.com>
2023-11-30 02:51:39 -0300

    Instalação Nodemon e Handlebars

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
     res.send("Hello, World!")
@@ -9,3 +13,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
+
