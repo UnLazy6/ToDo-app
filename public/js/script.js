0e27970342f1c1a15fdf874eaacbb5e0eb7c537a
UnLazy6 <caiovictortv6@gmail.com>
2023-11-30 22:35:45 -0300

    deixando a mudança de tema dinamica

---
+++ public/js/script.js
@@ -0,0 +1,54 @@
+function changeTheme() {
+    const theme = localStorage.getItem("theme")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".button-theme")
+
+    if (theme) {
+        let newTheme
+
+        if (theme === "light") {
+            newTheme = "dark"
+
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="sun icon">`
+
+            body.classList.remove("light")
+            body.classList.add("dark")
+
+        } else {
+            newTheme = "light"
+
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="moon icon">`
+
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("theme", newTheme)
+        return
+    }
+
+    localStorage.setItem("theme", "dark")
+    body.classList.add("dark")
+}
+
+function checkTheme() {
+    const theme = localStorage.getItem("theme")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".button-theme")
+
+    if (theme) {
+        if (theme === "dark") {
+            body.classList.add("dark")
+
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="sun icon">`
+
+        } else {
+            body.classList.add("light")
+
+            utton.innerHTML = `<img src="/imagens/moon-icon.png" alt="moon icon">`
+
+        }
+    }
+}
+
+checkTheme()
